be2c84cd9480324bbd1fd528bfbe6aa5568ddb6e
Tony Gorez <gorez.tony@gmail.com>
2017-10-25 22:03:38 +0200

    Business & BusinessList created

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+export class BusinessList extends React.Component {
+  render() {
+    return(
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    )
+  }
+}
